fac3769201aebebd146f73f202d0743e33da9fbf
Ted Lutkus <ted@onyx-robotics.com>
2026-06-10 13:26:21 -0400

    Release 0.1.3

--- src/developer.test.ts
+++ src/developer.test.ts
@@ -340,7 +340,7 @@ describe("developer mode", () => {
       })
     )
 
-    expect(output).toBe("0.1.2")
+    expect(output).toBe("0.1.3")
   })
 
   test("launcher reports missing linked dev files", async () => {
